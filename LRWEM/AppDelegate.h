00dfc7781aaad59fe16f8155f5662eda2003a8fb
Capital <303624150@qq.com>
2016-11-22 22:44:38 +0800

    Initial Commit

---
+++ LRWEM/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LRWEM
+//
+//  Created by rwli on 16/11/22.
+//  Copyright © 2016年 companyName. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LRWEM/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LRWEM
+//
+//  Created by rwli on 16/11/22.
+//  Copyright © 2016年 companyName. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
